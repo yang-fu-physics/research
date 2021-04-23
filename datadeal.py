b425a81ba07f2f9ea5576cfdd052b7f359b81bd9
fuyang0811 <1594567228@qq.com>
2021-04-23 23:21:10 +0800

    添加了readme。 添加了一些交互， 添加了R与rho的判断， 添加了图像的单位， 图像进行了合并和大小和边框调整。 添加了单独R的处理， 添加了中文逗号的支持。 修改了单一温度和两个温度无法处理的问题。

--- datadeal.py
+++ datadeal.py
@@ -4,8 +4,31 @@ from scipy import interpolate
 import numpy as np
 import matplotlib.pyplot as plt
 
-workdir = os.getcwd()
 
+"""
+使用origin删除掉废弃数据，最后留下四列（温度，磁场，电阻，霍尔）或者三列（温度，磁场，电阻）。保证每个温度，磁场都会从负值正值，或者正值到负值。
+使用origin导出ASCII，使用“，”作为分隔符。不要抬头，即输出文件只有数据。(如图)
+将程序和数据文件放入同一文件夹。注意需要只有一个dat文件。
+
+运行程序，初始化大概需要几秒。
+输入参数，若直接回车则是使用默认值。
+最后会生成一张图，分别为原始的R，Ryx。以及处理后的rho，rhoyx。若输入为三列数据，则只有R。可以按需要保存。若输入的样品尺寸为1，1，1，则rho会自动改为R。
+会生成R数据和hall数据。文件名并附有样品尺寸的信息，尺寸的逗号中英文皆可。
+生成的数据文件可直接拖入origin中。
+
+有时候会报警，因为内插时遇到了相同的x，和不同的y。python会只保留一个点。对最终结果没有影响。
+
+文件夹中的Sheet1.dat为示例文件。
+"""
+workdir = os.getcwd()
+def halltest(name):
+    a = open(name, "r+")
+    data = a.readlines()
+    line = data[0].strip().split(',') # strip()默认移除字符串首尾空格或换行符
+    if len(line)>3:
+        return True
+    else:
+        return False
 
 def plot(headline, data, rhoorhall):
     j = 0
@@ -14,8 +37,7 @@ def plot(headline, data, rhoorhall):
         j = j + 1
     plt.legend()
     plt.ylabel(rhoorhall)
-    plt.xlabel("Filed")
-    plt.show()
+    plt.xlabel("Filed(T)")
 
 
 def addheadline(headline, oldfile, newfile):
@@ -29,6 +51,7 @@ def addheadline(headline, oldfile, newfile):
 
 
 def Rtorho(data, abc):
+    abc = abc.replace("，", ",")
     abc = abc.strip().split(',')
     abcdeal = []
     for i in abc:
@@ -45,6 +68,7 @@ def Rtorho(data, abc):
 
 
 def Ryxtorhoyx(data, abc):
+    abc = abc.replace("，", ",")
     abc = abc.strip().split(',')
     abcdeal = []
     for i in abc:
@@ -87,8 +111,9 @@ def spit(dataT, j, range, lie, interval):
     return av
 
 
-def dealdata(name, range, lie, interval):
+def dealdata(name, range, lie, interval,plot):
     dataall = np.zeros([int(range*10000/interval+1), 40])
+    plt.subplot(plot)
     a = open(name, "r+")
     data = a.readlines()
     rows = len(data)  # 数据总行
@@ -127,23 +152,45 @@ def dealdata(name, range, lie, interval):
             j = Fchange[i] - Tchange[i]
             dataspit = spit(dataT, j, range, lie, interval)
             plt.plot(dataT[:, 1], dataT[:, 2], label="%.1f"% data2[Tchange[i - 1], 0] + "K")
-        elif i == 0:  # 第一组则取0：。
-            dataT = data2[:Tchange[i], :]
+            if lie == 3:
+                plt.ylabel("Ryx(ohm)")
+            else:
+                plt.ylabel("R(ohm)")
+            plt.xlabel("Filed(T)")
+            dataall[:, 0] = dataspit[:, 0]
+            dataall[:, i + 1] = dataspit[:, 1]
+        else:  # 第一组则取0：。
+            if Tchange==[]:
+                dataT=data2[:,:]
+            else:
+                dataT = data2[:Tchange[i], :]
             dataspit = spit(dataT, Fchange[0], range, lie, interval)
+            dataall[:, 0] = dataspit[:, 0]
+            dataall[:, i + 1] = dataspit[:, 1]
             plt.plot(dataT[:, 1], dataT[:, 2], label="%.1f"%data2[0, 0] + "K")
-        # print(i)
-        dataall[:, 0] = dataspit[:, 0]
-        dataall[:, i + 1] = dataspit[:, 1]
+            if lie == 3:
+                plt.ylabel("Ryx(ohm)")
+            else :
+                plt.ylabel("R(ohm)")
+            plt.xlabel("Filed(T)")
+            if Tchange==[]:
+                break
         if i == len(Tchange) - 1:  # 如果是最后一个点，则额外输出一个至最后的数组。并跳出循环
             dataT = data2[Tchange[i]:, :]
             dataspit = spit(dataT, Fchange[i + 1] - Tchange[i], range, lie, interval)
             dataall[:, 0] = dataspit[:, 0]
             dataall[:, i + 2] = dataspit[:, 1]
-            plt.plot(dataT[:, 1], dataT[:, 2], label="%.1f"%data2[Tchange[i], 0] + "K")
+            plt.plot(dataT[:, 1], dataT[:, 2], label="%.1f" % data2[Tchange[i], 0] + "K")
+            if lie == 3:
+                plt.ylabel("Ryx(ohm)")
+            else:
+                plt.ylabel("R(ohm)")
+            plt.xlabel("Filed(T)")
             break
+        # print(i)
+
         i = i + 1
     plt.legend()
-    plt.show()
     Tchange.insert(0, int(0))
     return dataall, data2[Tchange, 0]
 
@@ -165,24 +212,52 @@ else:
     else:
         interval = float(interval)
     print("内插间隔为%.0d"%interval)
-    abc = input("输入长宽高，英文逗号隔开，单位为cm，回车则皆为1，即输出为电阻\n")
+    abc = input("输入长宽高，逗号隔开，单位为cm，回车则皆为1，即输出为电阻\n")
     if abc == "":
         abc = "1,1,1"
     print("长宽高分别为"+abc)
     input("确认参数")
-    [dataR, headline] = dealdata(file[0], range, 2, interval)
+
+
+    #R处理
+    if halltest(file[0]):
+        plt.figure(figsize=(16, 9))
+        [dataR, headline] = dealdata(file[0], range, 2, interval,221)
+    else:
+        plt.figure(figsize=(8, 9))
+        [dataR, headline] = dealdata(file[0], range, 2, interval, 211)
     dataR = dataR.T[~(dataR == 0).all(0)].T  # 去除0列
     dataR = Rtorho(dataR, abc)
     np.savetxt("dealed-R.dat", dataR, fmt="%.8e", delimiter=",")
-    headlinestr = "Filed"
+    headlinestr = "Filed(T)"
     for i in headline:
-        headlinestr = headlinestr + "," + "%.1f"%i + "K"
+        if abc.replace("，", ",") == "1,1,1":
+            headlinestr = headlinestr + "," + "%.1f"%i + "K(ohm)"
+        else:
+            headlinestr = headlinestr + "," + "%.1f" % i + "K(ohm cm)"
     addheadline(headlinestr, "dealed-R.dat", "dealed-R-" + abc + ".dat")
-    [datahall, headline] = dealdata(file[0], range, 3, interval)
-    datahall = datahall.T[~(datahall == 0).all(0)].T  # 去除0列
-    datahall = Ryxtorhoyx(datahall, abc)
-    np.savetxt("dealed-hall.dat", datahall, fmt="%.8e", delimiter=",")
-    addheadline(headlinestr, "dealed-hall.dat", "dealed-hall-" + abc + ".dat")
-    plot(headline, dataR, "R")
-    plot(headline, datahall, "hall")
+    if halltest(file[0]):
+        plt.subplot(223)
+    else:
+        plt.subplot(212)
+    if abc.replace("，", ",")=="1,1,1":
+        plot(headline, dataR, "R(ohm)")
+    else:
+        plot(headline, dataR, "rho(ohm cm)")
+    #hall处理
+    if halltest(file[0]):
+        [datahall, headline] = dealdata(file[0], range, 3, interval,222)
+        datahall = datahall.T[~(datahall == 0).all(0)].T  # 去除0列
+        datahall = Ryxtorhoyx(datahall, abc)
+        np.savetxt("dealed-hall.dat", datahall, fmt="%.8e", delimiter=",")
+        addheadline(headlinestr, "dealed-hall.dat", "dealed-hall-" + abc + ".dat")
+        plt.subplot(224)
+        if abc.replace("，", ",") == "1,1,1":
+            plot(headline, datahall, "Ryx(ohm)")
+        else:
+            plot(headline, datahall, "rhoyx(ohm cm)")
+
+
+    plt.tight_layout()
+    plt.show()
 input("by fuyang ヽ(°∀°)ﾉ  \n 按任意键结束")
